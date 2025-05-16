1ccbc0cd6646d1cf6a38b4aced5428bb6d5b72e1
Saba <imnadzesaba7@gmail.com>
2025-05-16 09:07:14 +0400

    .

---
+++ GetData.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace async1;
+
+public class GetData
+{
+    public static async Task<string> GetApiData(Uri uri)
+    {
+        using (HttpClient client = new HttpClient(){BaseAddress = uri})
+        {
+            var msg = await client.GetStringAsync("");
+            return msg;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace async1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            List<Uri> uris = 
+            [
+                new Uri("https://car-api-htd7gjh9brb8asgp.uksouth-01.azurewebsites.net/api/car/car"),
+                new Uri("https://gallery-api-3-e2a9g0bkdnfwfrg8.uksouth-01.azurewebsites.net/api/main"),
+            ];
+            var tasks = uris.Select(async u =>
+            {
+                Console.WriteLine("awaiting data...");
+                string c = await GetData.GetApiData(u);
+                return c;
+            }).ToArray();
+            var results = await Task.WhenAll(tasks);
+            foreach (var c in results)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
+
